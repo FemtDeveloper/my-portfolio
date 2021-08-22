17408ea4739237e8cf795d541d580564870ee086
Felix Miranda <feedmite@hotmail.com>
2021-08-22 02:32:17 -0500

    new changes

---
+++ portfolio/src/components/navbar/MobileNavbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MobileNavbar.css";
+
+const MobileNavbar = () => {
+  return (
+    <div className="mobile-navbar">
+      <a href="">Home</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="">Contact</a>
+    </div>
+  );
+};
+
+export default MobileNavbar;
---
+++ portfolio/src/portafolioProjects/todoApp/components/todoList/TodoList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import TodoItem from "../todoItem/TodoItem";
+import "./TodoList.css";
+
+const TodoList = () => {
+  const todoList = useSelector((state) => state.todos);
+
+  return (
+    <div className="todoList--container">
+      {todoList.map((todo) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            description={todo.description}
+          />
+        );
+        X;
+      })}
+    </div>
+  );
+};
+
+export default TodoList;
