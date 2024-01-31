f36177a63e4aa76896515616991ec65f45c1d1fc
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2024-01-31 18:48:58 -0500

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Route, Switch, useLocation } from "react-router-dom";
-import Calc from "./portafolioProjects/calc/Calc";
-import TodoApp from "./portafolioProjects/todoApp/TodoApp";
-import Home from "./components/home/Home";
-import Header from "./components/header/Header";
-import { AnimatePresence } from "framer-motion";
 
 function App() {
   const location = useLocation();
   return (
     <div className="App">
-      <Header />
-      {/* <AnimatePresence exitBeforeEnter> */}
-      <Switch location={location} key={location.key}>
-        <Route path="/todoApp">
-          <TodoApp />
-        </Route>
-        <Route path="/calc">
-          <Calc />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-      {/* </AnimatePresence> */}
+      <h1>Page under construction</h1>
     </div>
   );
 }
